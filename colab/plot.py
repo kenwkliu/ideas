c7dd09ceee6539d221b7b74ce72b7d634c7a4d3f
kenwkliu <wkliu_x@yahoo.com>
2021-05-18 07:18:36 +0800

    Add files via upload

---
+++ colab/plot.py
@@ -0,0 +1,29 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+# Plot the OHLCV chart
+def ohlcv(df):
+	rDf = df.reset_index()
+
+	# Create subplots and mention plot grid size
+	fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
+				   vertical_spacing=0.03, 
+				   subplot_titles=('OHLC', 'Volume'), 
+				   row_width=[0.2, 0.7])
+
+	fig.add_trace(go.Ohlc(x=rDf['Date'],
+					  open=rDf['Open'],
+					  high=rDf['High'],
+					  low=rDf['Low'],
+					  close=rDf['Close'],
+					  name="OHLC"),
+					  row=1, col=1)
+
+	# Bar trace for volumes on 2nd row without legend
+	fig.add_trace(go.Bar(x=rDf['Date'], y=rDf['Volume'], showlegend=False), row=2, col=1)
+
+	# Do not show OHLC's rangeslider plot 
+	fig.update(layout_xaxis_rangeslider_visible=False)
+
+	fig.show()
